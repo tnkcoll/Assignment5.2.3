0e4a8e91f1af55a63ac76ad8bb34598f5bed1241
tnkcoll <theodore.collins2023@outlook.com>
2024-06-04 15:56:39 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Assignment5._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = GetNumberFromUser("Please enter a number.");
+            GetNaturalNumbers(number);
+        }
+
+        static int GetNumberFromUser(string s)
+        {
+            string? userInput = "";
+
+            do
+            {
+                Console.WriteLine(s);
+                userInput = Console.ReadLine();
+            } while (userInput == "");
+            return Convert.ToInt32(userInput);
+        }
+
+        static void GetNaturalNumbers(int num)
+        {
+            if (num > 0)
+            {
+                Console.Write($"{num} ");
+                GetNaturalNumbers(num - 1);
+            }
+        }
+    }
+}
